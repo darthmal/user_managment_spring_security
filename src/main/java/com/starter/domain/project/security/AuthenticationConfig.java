0129b959f4308c44c144734d7d6ebebaaaa3ec99
Salam <salamabou88@gmail.com>
2025-03-12 23:51:26 +0100

    feat: implementing spring security

---
+++ src/main/java/com/starter/domain/project/security/AuthenticationConfig.java
@@ -0,0 +1,17 @@
+package com.starter.domain.project.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
